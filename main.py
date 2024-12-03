a8d7e197e2ad2da3ecbf95e360dc34a580ec79d2
ahmed77mostafa <ahmedmostafa.0522030@gmail.com>
2024-12-03 10:11:03 +0200

    Normal CV image processing

--- main.py
+++ main.py
@@ -10,22 +10,6 @@ print(numpy.__version__)
 print(arr2)
 
 image = cv2.imread("D:\Wallpaper\dead guy.jpg")
-# Loading the image
-
-half = cv2.resize(image, (0, 0), fx = 0.1, fy = 0.1)
-bigger = cv2.resize(image, (1050, 1610))
-
-stretch_near = cv2.resize(image, (780, 540),
-               interpolation = cv2.INTER_LINEAR)
-
-
-Titles =["Original", "Half", "Bigger", "Interpolation Nearest"]
-images =[image, half, bigger, stretch_near]
-count = 4
-
-for i in range(count):
-    plt.subplot(2, 2, i + 1)
-    plt.title(Titles[i])
-    plt.imshow(images[i])
-
-plt.show()
+cv2.imshow("dead guy",image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
